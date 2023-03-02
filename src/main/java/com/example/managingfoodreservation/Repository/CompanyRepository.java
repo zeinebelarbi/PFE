66caeadaed944a0de0f850326be579e51cf5b976
Zeineb El arbi <77177950+zeinebelarbi@users.noreply.github.com>
2023-03-02 10:14:12 +0100

    Delete CompanyRepository.java

--- src/main/java/com/example/managingfoodreservation/Repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.managingfoodreservation.Repository;
-
-import com.example.managingfoodreservation.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company,Integer> {
-}
