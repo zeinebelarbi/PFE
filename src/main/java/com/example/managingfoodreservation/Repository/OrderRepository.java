0d8fd45b7763ce84b36a657e531a08bf70327700
Zeineb El arbi <77177950+zeinebelarbi@users.noreply.github.com>
2023-03-02 10:15:13 +0100

    Update OrderRepository.java

--- src/main/java/com/example/managingfoodreservation/Repository/OrderRepository.java
+++ src/main/java/com/example/managingfoodreservation/Repository/OrderRepository.java
@@ -4,4 +4,7 @@ import com.example.managingfoodreservation.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository< Order,Integer> {
+ Optional<Order> findById(Integer id);
+ Optional <Order> findByDate(Instant date);
+ Optional <Order> findByName(String name);
 }
