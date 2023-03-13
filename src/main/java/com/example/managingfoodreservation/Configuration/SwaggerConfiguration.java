c97329b77c0945499d1e96351380903ad5aa8b66
zeinebelarbi <zeineb.elarbi@enis.tn>
2023-03-13 08:40:30 +0100

    Adding Files

--- src/main/java/com/example/managingfoodreservation/Configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/managingfoodreservation/Configuration/SwaggerConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.managingfoodreservation.Configuration;
 
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
